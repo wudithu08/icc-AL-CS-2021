3db75a5a494432572750343a8d773f846a44f8c8
CathyYang1118 <50823016+CathyYang1118@users.noreply.github.com>
2019-09-23 15:31:43 +0800

    Update Q5.py

--- Assessments/files/Q5.py
+++ Assessments/files/Q5.py
@@ -1,15 +1,14 @@
 FileWrite = open('HealthCare.txt','w')
 
-end = '' #string
+end = '' #STRING
 
 while end != '!':
-    name = input('Input name:') #string
-    height = input('Input height:') #string
-    weight = input('Input weight:') #string
-    VitalCapacity = input('Input vital capacity:') #string
-    WriteInformation = name + ' ' + height + ' ' + weight + ' ' + VitalCapacity + '\n' #string
+    name = input('Input name:') #STRING
+    height = input('Input height:') #STRING
+    weight = input('Input weight:') #STRING
+    VitalCapacity = input('Input vital capacity:') #STRING
+    WriteInformation = name + ' ' + height + ' ' + weight + ' ' + VitalCapacity + '\n' #STRING
     FileWrite.write(WriteInformation)
-    end = input('If you want to stop inputting information,please input"!":')
+    end = input('If you want to stop inputting information,please input"!":')#STRING
 
 FileWrite.close()
-    
